6c0eaacab82a610ff89187353ff3e3b6c45edb25
Pufferfish <puffish@puffish.net>
2024-06-22 21:34:54 +0200

    Remove unused code

--- Common/src/main/java/net/puffish/skillsmod/access/EntityAttributeInstanceAccess.java
+++
@@ -1,6 +0,0 @@
-package net.puffish.skillsmod.access;
-
-public interface EntityAttributeInstanceAccess {
-	double computeIncreasedValueForInitial(double initial);
-	double computeDecreasedValueForInitial(double initial);
-}
